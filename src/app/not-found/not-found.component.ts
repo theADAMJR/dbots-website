79f2a11386839377759159b67739e9bac59ead53
theADAMJR <adamjf123@gmail.com>
2020-06-05 14:49:45 +0100

    first

--- src/app/not-found/not-found.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-
-@Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
-})
-export class NotFoundComponent {}
--- src/app/utils.ts
+++
@@ -1,6 +0,0 @@
-export function toIterable(length: number) {
-  const array = [];
-  for (let i = 0; i < length; i++)
-    array.push(i);
-  return array;
-}
