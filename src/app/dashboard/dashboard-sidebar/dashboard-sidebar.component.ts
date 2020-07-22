d3dd9ac1967e0d98d45d71fc3e0a1dbcf3fee66c
theADAMJR <adamjf123@gmail.com>
2020-07-22 20:07:01 +0100

    add theme updating

--- src/app/dashboard/dashboard-sidebar/dashboard-sidebar.component.ts
+++ src/app/dashboard/dashboard-sidebar/dashboard-sidebar.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { ThemeService } from 'src/app/services/theme.service';
+import { MatSelect } from '@angular/material/select';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'dashboard-sidebar',
   templateUrl: './dashboard-sidebar.component.html',
   styleUrls: ['./dashboard-sidebar.component.css']
 })
-export class DashboardSidebarComponent {
+export class DashboardSidebarComponent implements OnInit {
+  @ViewChild('themeSelect') themeSelect: MatSelect;
+
+  defaultTheme = 'DISCORD';
+
+  form = new FormGroup({
+    theme: new FormControl(localStorage.getItem('theme') ?? this.defaultTheme)
+  });
+  
   get user() { return this.userService.user ?? {}; }
 
-  constructor(private userService: UserService) {
+  constructor(
+    private service: ThemeService,
+    private userService: UserService) {
     document.title = 'DBots - Dashboard';
+  }
+  ngOnInit() {
+    this.service.updateTheme();
+    this.form.valueChanges.subscribe(() => 
+      this.service.changeTheme(
+        this.form.get('theme').value));
   }
 }
